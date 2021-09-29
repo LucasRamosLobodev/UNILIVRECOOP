b43979608a2dd76e6fbe13a0fb606518ae8de1b0
LUCASRAMOSLOBO <88864017+LUCASRAMOSLOBO@users.noreply.github.com>
2021-09-29 09:57:34 -0300

    make it better

--- project/settings.py
+++ project/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y=(ebl4aq_aso)msecret key 2z6a04c486b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['maisteste2.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
